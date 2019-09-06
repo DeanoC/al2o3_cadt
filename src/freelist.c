67e8a39d13634eb6c4cf03b8d649dceaaab1c96e
Deano <me@deanoc.com>
2019-09-06 14:00:07 +0300

    Freelist passes basic test, reduced to one alloc from two More test and fixes for image compression lib Reverted back to double AMD bc7 compressor Fixed bugs in depth/stencil formats and queries in TinyImageFormat Fixed TinyImageFormat query tests Fixed tinykyx tests

--- src/freelist.c
+++ src/freelist.c
@@ -10,7 +10,6 @@ typedef struct CADT_FreeList {
 	size_t capacity;
 	size_t size;
 
-	uint8_t* data;
 	uintptr_t headIndex;
 
 } CADT_FreeList;
@@ -21,71 +20,67 @@ AL2O3_EXTERN_C CADT_FreeListHandle CADT_FreeListCreate(size_t elementSize, size_
 	ASSERT(elementSize >= sizeof(uintptr_t));
 	ASSERT(capacity > 0);
 
-	CADT_FreeList* freelist = MEMORY_CALLOC(1, sizeof(CADT_FreeList));
-	if(freelist == NULL) return NULL;
-	freelist->elementSize = elementSize;
-
-	freelist->data = (uint8_t*)MEMORY_MALLOC(sizeof(elementSize) * capacity);
-	if(freelist->data == NULL) {
-		MEMORY_FREE(freelist);
+	size_t const size = ((sizeof(CADT_FreeList) + sizeof(elementSize) * capacity) + sizeof(uint64_t))/ sizeof(uint64_t);
+	CADT_FreeList *freelist = MEMORY_CALLOC(1, size);
+	if (freelist == NULL)
 		return NULL;
-	}
-
-	for (size_t i = 0; i < capacity-1;++i) {
-		uintptr_t* data = (uintptr_t*)(freelist->data + (i * freelist->elementSize));
-		*data = i+1;
-	}
-	uintptr_t* data = (uintptr_t*)(freelist->data + ((capacity-1) * freelist->elementSize));
-	*data = END_OF_LIST_SENTINEL;
-
+	freelist->elementSize = elementSize;
 	freelist->headIndex = 0;
 	freelist->capacity = capacity;
+
+	uint8_t *data = (uint8_t *) (freelist + 1);
+	for (size_t i = 0; i < capacity - 1; ++i) {
+		*(uintptr_t *) data = i + 1;
+		data += freelist->elementSize;
+	}
+	*(uintptr_t *) data = END_OF_LIST_SENTINEL;
 
 	return freelist;
 }
 
 AL2O3_EXTERN_C void CADT_FreeListDestroy(CADT_FreeListHandle handle) {
 	ASSERT(handle != NULL);
-	CADT_FreeList* freelist = (CADT_FreeList*)handle;
-	if(freelist->data != NULL) {
-		MEMORY_FREE(freelist->data);
-	}
+	CADT_FreeList *freelist = (CADT_FreeList *) handle;
 	MEMORY_FREE(freelist);
 }
 
 AL2O3_EXTERN_C CADT_FreeListHandle CADT_FreeListClone(CADT_FreeListHandle handle) {
 	ASSERT(handle != NULL);
-	CADT_FreeList* ofreelist = (CADT_FreeList*)handle;
-	CADT_FreeList* nfreelist = (CADT_FreeList*)CADT_FreeListCreate(ofreelist->elementSize, ofreelist->capacity);
-	if (nfreelist == NULL) return NULL;
-	memcpy(nfreelist->data, ofreelist->data, ofreelist->size * ofreelist->elementSize);
-	nfreelist->headIndex = ofreelist->headIndex;
+	CADT_FreeList const *ofreelist = (CADT_FreeList const *) handle;
+	size_t const size = sizeof(CADT_FreeList) + sizeof(ofreelist->elementSize) * ofreelist->capacity;
 
-	return nfreelist;
+	CADT_FreeList *freelist = MEMORY_MALLOC(size);
+	if (freelist == NULL)
+		return NULL;
+
+	memcpy(freelist, ofreelist, size);
+
+	return freelist;
 }
 
-AL2O3_EXTERN_C void* CADT_FreeListAlloc(CADT_FreeListHandle handle) {
+AL2O3_EXTERN_C void *CADT_FreeListAlloc(CADT_FreeListHandle handle) {
 	ASSERT(handle != NULL);
-	CADT_FreeList* freelist = (CADT_FreeList*)handle;
+	CADT_FreeList *freelist = (CADT_FreeList *) handle;
 
 	if (freelist->headIndex == END_OF_LIST_SENTINEL) {
 		return NULL;
 	}
-
-	void* data = freelist->data + (freelist->headIndex * freelist->elementSize);
-	freelist->headIndex = *(uintptr_t*)data;
+	uint8_t *data = (uint8_t *) (freelist + 1);
+	data = data + (freelist->headIndex * freelist->elementSize);
+	freelist->headIndex = *(uintptr_t *) data;
 
 	return data;
 }
 
-AL2O3_EXTERN_C void CADT_FreeListFree(CADT_FreeListHandle handle, void* ptr) {
+AL2O3_EXTERN_C void CADT_FreeListFree(CADT_FreeListHandle handle, void *ptr) {
 	ASSERT(handle != NULL);
 	ASSERT(ptr != NULL);
-	CADT_FreeList* freelist = (CADT_FreeList*)handle;
+	CADT_FreeList *freelist = (CADT_FreeList *) handle;
 
-	ASSERT((uint8_t*)ptr >= freelist->data);
-	ASSERT((uint8_t*)ptr < freelist->data + freelist->elementSize * freelist->capacity);
+	uint8_t *data = (uint8_t *) (freelist + 1);
+	ASSERT((uint8_t *) ptr >= data);
+	ASSERT((uint8_t *) ptr < data + freelist->elementSize * freelist->capacity);
 
-	*((uintptr_t*)ptr) = freelist->headIndex;
-	freelist->headIndex = ((uint8_t*)ptr - freelist->data) / freelist->elementSize;
+	*((uintptr_t *) ptr) = freelist->headIndex;
+	freelist->headIndex = ((uint8_t *) ptr - data) / freelist->elementSize;
 }
