69d058724d4d41d0e5d5540deed1070e7f4c9ae9
Deano <me@deanoc.com>
2019-10-01 18:34:58 +0300

    working out work pipeline for writing unit tests...

--- tests/test_freelist.cpp
+++ tests/test_freelist.cpp
@@ -16,3 +16,20 @@ TEST_CASE("CADT_FreeList Single Alloc/Free", "[CADT CADT_FreeList]") {
 	REQUIRE(*d == 1);
 	CADT_FreeListDestroy(handle);
 }
+
+TEST_CASE("CADT_FreeList Multiple Alloc/Free", "[CADT CADT_FreeList]") {
+	CADT_FreeListHandle handle = CADT_FreeListCreate(sizeof(uintptr_t)+2, 10);
+	REQUIRE(handle);
+	uintptr_t* d0 = (uintptr_t*) CADT_FreeListAlloc(handle);
+	*d0 = 42;
+	uintptr_t* d1 = (uintptr_t*) CADT_FreeListAlloc(handle);
+	*d1 = 3;
+	uintptr_t* d2 = (uintptr_t*) CADT_FreeListAlloc(handle);
+	*d2 = 13;
+
+	CADT_FreeListFree(handle, d2);
+	CADT_FreeListFree(handle, d1);
+	CADT_FreeListFree(handle, d0);
+
+	CADT_FreeListDestroy(handle);
+}
